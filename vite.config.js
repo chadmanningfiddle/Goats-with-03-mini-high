8868aac0b85d5e071facb13602c29aa369401f67
chadmanningfiddle <Chadmanning@gmail.com>
2025-02-12 04:54:53 +0000

    cross your nose hairs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './public/dist',
-    assetsDir: 'assets',
-    manifest: true
-  },
   server: {
     port: 5173,
+    strictPort: true,
+    open: false,
     proxy: {
       '/api': 'http://localhost:3000'
     }
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+  build: {
+    outDir: 'public/dist',
+    assetsDir: 'assets',
+    manifest: true
   }
-});
+})
